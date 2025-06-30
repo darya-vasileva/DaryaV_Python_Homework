6b54a220c5af618540098111ba04a52ceea81e28
Daria V <darya.vasileva.93@gmail.com>
2025-06-30 20:49:18 +0300

    Добавлены файлы с заданиями

---
+++ lesson_07/shop_Main.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class ShopMainPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def add_item(self, product_name):
+        WebDriverWait(self.driver, 10)
+        add_button = self.driver.find_element(
+            By.CSS_SELECTOR, f"[data-test='{f"add-to-cart-{product_name}"}']")
+        add_button.click()
+
+    def shopping_cart(self):
+        self.cart = self.driver.find_element(
+            By.CLASS_NAME, "shopping_cart_link")
+        self.cart.click()
